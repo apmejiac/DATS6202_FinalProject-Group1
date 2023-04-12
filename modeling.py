3337d2f0d8b30e6ae4a690eaa0e7bc8f1b15884e
awinkhater <112085352+awinkhater@users.noreply.github.com>
2023-04-12 12:25:11 -0400

    Made a naive bayes model

--- modeling.py
+++ modeling.py
@@ -23,19 +23,19 @@ scaled_data = scaler.transform(df)
 pca = PCA(n_components=28)
 pca.fit(scaled_data)
 x_pca = pca.transform(scaled_data)
-print(scaled_data.shape,x_pca.shape)
+#print(scaled_data.shape,x_pca.shape)
 #Graph to see how many Components are needed
 exp_var_cumul = np.cumsum(pca.explained_variance_ratio_)
-fig=px.area(
-    x=range(1, exp_var_cumul.shape[0] + 1),
-    y=exp_var_cumul)
-fig.update_layout(xaxis_title='# of Component', yaxis_title='Explained Variance')
-fig.show()
+# fig=px.area(
+#     x=range(1, exp_var_cumul.shape[0] + 1),
+#     y=exp_var_cumul)
+# fig.update_layout(xaxis_title='# of Component', yaxis_title='Explained Variance')
+# fig.show()
 # We need 20 Components to explain .97 of varaince, thus dropping 8 dimensions
 pca = PCA(n_components=20)
 pca.fit(scaled_data)
 x_pca = pca.transform(scaled_data)
-print(scaled_data.shape,x_pca.shape)
+#print(scaled_data.shape,x_pca.shape)
 #Setting pca_df as to get it ready for modeling
 df_pca= x_pca
 y= DF['tsunami'].values
@@ -48,7 +48,7 @@ X_train, X_test, y_train, y_test = train_test_split(df_pca, y, test_size=0.33, r
 from sklearn.neural_network import MLPClassifier
 
 # Initializing the multilayer perceptron
-mlp = MLPClassifier(hidden_layer_sizes=10,solver='lbfgs',learning_rate_init= 0.01, max_iter=500)
+mlp = MLPClassifier(hidden_layer_sizes=10,solver='lgsb',learning_rate_init= 0.01, max_iter=500)
 
 mlp.fit(X_train, y_train)
 
@@ -56,8 +56,7 @@ print (mlp.score(X_test,y_test))
 
 from sklearn.metrics import confusion_matrix
 y_pred = mlp.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
+
 
 from sklearn.metrics import plot_confusion_matrix
 #Pretty CM Graph
@@ -69,7 +68,20 @@ plt.ylabel('True Label', color=color)
 plt.gcf().axes[0].tick_params(colors=color)
 plt.gcf().axes[1].tick_params(colors=color)
 plt.show()
-
+#
+# #NB Model
+# from sklearn.naive_bayes import GaussianNB
+# gnb = GaussianNB()
+# y_pred = gnb.fit(X_train, y_train).predict(X_test)
+# #NB CM
+# color = 'white'
+# matrix = plot_confusion_matrix(gnb, X_test, y_test, cmap=plt.cm.Blues)
+# matrix.ax_.set_title('Confusion Matrix2', color=color)
+# plt.xlabel('Predicted Label', color=color)
+# plt.ylabel('True Label', color=color)
+# plt.gcf().axes[0].tick_params(colors=color)
+# plt.gcf().axes[1].tick_params(colors=color)
+# plt.show()
 
 
 

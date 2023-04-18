7847b3528b89ff3096a8082c34c355929f829f1c
awinkhater <112085352+awinkhater@users.noreply.github.com>
2023-04-18 12:13:40 -0400

    Did Statistics and Created a csv

--- modeling.py
+++ modeling.py
@@ -5,59 +5,29 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import plotly.express as px
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+df=pd.read_csv('DF_PREPPED.csv.')
+x=df.drop(['tsunami'], axis=1)
+x=x.drop(x.columns[0], axis=1)
+y= df['tsunami'].values
 
-df=pd.read_csv('EQ_Clean.csv')
-#Original Dataset in case we want to save dat_time
-DF=df.copy()
-#Dropping non-useful variables
-df= df.drop(['title','date_time', 'location'], axis=1)
-# #Encoding Categorical Variables
-cat_df = df.select_dtypes(include=['object']).copy()
-df[cat_df.columns] = df[cat_df.columns].astype(str)
-df = pd.get_dummies(df)
-#Normalizing Data
-scaler = StandardScaler()
-scaler.fit(df)
-scaled_data = scaler.transform(df)
-#PCA
-pca = PCA(n_components=28)
-pca.fit(scaled_data)
-x_pca = pca.transform(scaled_data)
-#print(scaled_data.shape,x_pca.shape)
-#Graph to see how many Components are needed
-exp_var_cumul = np.cumsum(pca.explained_variance_ratio_)
-# fig=px.area(
-#     x=range(1, exp_var_cumul.shape[0] + 1),
-#     y=exp_var_cumul)
-# fig.update_layout(xaxis_title='# of Component', yaxis_title='Explained Variance')
-# fig.show()
-# We need 20 Components to explain .97 of varaince, thus dropping 8 dimensions
-pca = PCA(n_components=20)
-pca.fit(scaled_data)
-x_pca = pca.transform(scaled_data)
-#print(scaled_data.shape,x_pca.shape)
-#Setting pca_df as to get it ready for modeling
-df_pca= x_pca
-y= DF['tsunami'].values
 
 #Train Test Split:
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(df_pca, y, test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.33, random_state=42)
 
 from sklearn.neural_network import MLPClassifier
 
 # Initializing the multilayer perceptron
-mlp = MLPClassifier(hidden_layer_sizes=10,solver='lgsb',learning_rate_init= 0.01, max_iter=500)
+mlp = MLPClassifier(hidden_layer_sizes=10,solver='sgd',learning_rate_init= 0.01, max_iter=500)
 
 mlp.fit(X_train, y_train)
 
-print (mlp.score(X_test,y_test))
 
 from sklearn.metrics import confusion_matrix
 y_pred = mlp.predict(X_test)
-
-
 from sklearn.metrics import plot_confusion_matrix
 #Pretty CM Graph
 color = 'white'
@@ -70,18 +40,86 @@ plt.gcf().axes[1].tick_params(colors=color)
 plt.show()
 #
 # #NB Model
-# from sklearn.naive_bayes import GaussianNB
-# gnb = GaussianNB()
-# y_pred = gnb.fit(X_train, y_train).predict(X_test)
-# #NB CM
-# color = 'white'
-# matrix = plot_confusion_matrix(gnb, X_test, y_test, cmap=plt.cm.Blues)
-# matrix.ax_.set_title('Confusion Matrix2', color=color)
-# plt.xlabel('Predicted Label', color=color)
-# plt.ylabel('True Label', color=color)
-# plt.gcf().axes[0].tick_params(colors=color)
-# plt.gcf().axes[1].tick_params(colors=color)
-# plt.show()
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+y_pred = gnb.fit(X_train, y_train).predict(X_test)
+#NB CM
+color = 'white'
+matrix = plot_confusion_matrix(gnb, X_test, y_test, cmap=plt.cm.Blues)
+matrix.ax_.set_title('Confusion Matrix2', color=color)
+plt.xlabel('Predicted Label', color=color)
+plt.ylabel('True Label', color=color)
+plt.gcf().axes[0].tick_params(colors=color)
+plt.gcf().axes[1].tick_params(colors=color)
+plt.show()
+#
+print("MLP:",mlp.score(X_test, y_test))
+print("GNB:",gnb.score(X_test, y_test))
 
+#TSME
+#Try RF
+#PCA not necessary
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.metrics import  accuracy_score
+model = xgb.XGBClassifier()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+predictions = [round(value) for value in y_pred]
 
+from sklearn.model_selection import GridSearchCV
+#Hyperparameter Tuning
+param_grid = {
+    "max_depth": [3, 4, 5, 7],
+    "learning_rate": [0.1, 0.01, 0.05],
+    "gamma": [0, 0.25, 1],
+    "reg_lambda": [0, 1, 10],
+    "scale_pos_weight": [1, 3, 5],
+    "subsample": [0.8],
+    "colsample_bytree": [0.5]}
+#Init classifier
+xgb_cl = xgb.XGBClassifier(objective="binary:logistic")
 
+#Init Grid Search
+grid_cv = GridSearchCV(xgb_cl, param_grid, n_jobs=-1, cv=3, scoring="roc_auc")
+
+# Fit
+
+_ = grid_cv.fit(X_train, y_train)
+print("Grid CV1 best score:",grid_cv.best_score_)
+
+print(grid_cv.best_params_)
+#New Parameter ranges since some are at their end
+param_grid2 = {
+    "max_depth": [8,9, 15, 20],
+    "learning_rate": [0.2, 0.4, 0.8],
+    "gamma": [1.5,2, 3, 5],
+    "reg_lambda": [0.25, 0.5, 0.75, 1, 2],
+    "scale_pos_weight": [1, 3, 5],
+    "subsample": [0.8],
+    "colsample_bytree": [0.5]
+}
+
+grid_cv_2 = GridSearchCV(xgb_cl, param_grid2,
+                         cv=3, scoring="roc_auc", n_jobs=-1)
+
+_ = grid_cv_2.fit(X_train, y_train)
+print("gcv2 best score:",grid_cv_2.best_score_)
+#Final XGB classifier
+final_cl = xgb.XGBClassifier(
+    **grid_cv_2.best_params_,
+    objective="binary:logistic",
+)
+
+_ = final_cl.fit(X_train, y_train)
+from sklearn.metrics import roc_auc_score
+A= roc_auc_score(y_test, final_cl.predict_proba(X_test)[:, 1])
+print(A)
+color = 'white'
+matrix = plot_confusion_matrix(final_cl, X_test, y_test, cmap=plt.cm.Blues)
+matrix.ax_.set_title('Confusion Matrix2', color=color)
+plt.xlabel('Predicted Label', color=color)
+plt.ylabel('True Label', color=color)
+plt.gcf().axes[0].tick_params(colors=color)
+plt.gcf().axes[1].tick_params(colors=color)
+plt.show()
